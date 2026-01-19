8109808fbaf9a1f1c0250827f5bf3966c5111425
terrtam <97931134+terrtam@users.noreply.github.com>
2026-01-19 16:19:29 -0500

    package changes

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,9 @@
-import 'dotenv/config';
-import * as path from 'path';
-import { defineConfig, env } from 'prisma/config';
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: path.join('prisma', 'schema.prisma'),
-
+  schema: "prisma/schema.prisma",
   datasource: {
-    url: env('DATABASE_URL'),
-  },
-
-  migrations: {
-    seed: "ts-node prisma/seed.ts"
+    url: env("DATABASE_URL"),
   },
 });
-
